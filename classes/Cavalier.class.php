056fb65f9e9fa73298c89d2bc53a6d0922f18138
jbAfpa <jea.vau@dwwm.afpa>
2025-12-10 11:26:38 +0100

    Cavalier init

---
+++ classes/Cavalier.class.php
@@ -0,0 +1,57 @@
+<?php 
+
+    // classes/PieceEchecs.class.php
+    class Cavalier {
+        // Constante 
+
+        // Propriétés
+
+
+        // Constructeur
+        
+        /**
+         * Construit l'objet
+         * 
+         * 
+         */
+        // blabla 
+
+
+        // Accesseurs (getters, setters)
+
+        // SETTER
+
+        /**
+         * Modifie la coordonée de 
+         *
+         * @param #
+         * @return #
+         */
+
+        // public function set# (#): truc
+        // {
+            
+        //     return $this;
+        // }
+
+
+
+        // GETTER
+
+       
+        /**
+         * Get the value of 
+         */
+        // public function get#(): truc
+        // {
+        //     return $this->#;
+        // }
+
+            
+
+        // Autres méthodes
+
+       
+
+    }
+?>
